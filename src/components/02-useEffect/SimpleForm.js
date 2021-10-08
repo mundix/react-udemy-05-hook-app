723e4c046609a17d811f35f6e8cd84c6874a3388
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-08 04:21:03 -0400

    Working with useEffects with simple form

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '', 
+        email: ''
+    })
+    const { name, email} = formState;
+
+    const handleInputChange = ({target}) => {
+        // console.log(e.target);
+        setFormState({
+            ...formState,
+            // esto genera el nombre del input, en este caso 'name' que apunta a la propeidad name del obj del state
+            //el formState = {name, email}, y el valor del [target.name] = 'name 
+            [target.name]: target.value
+        })
+    }
+
+    // El use Effect es un hook que nos permite ejecuter un efecto secundario, cuando algo suceda en el component
+    useEffect(() => {
+        // Cada ves qeu suceda un cambio en e mismo , no importa que cambie , vuelvete a ejecutar 
+        // En el caso del input se ejecuta tantas vecs toque el teclado en input par escribir 
+        console.log('hey. corri una sola vez');
+    // });
+    }, []); //Esto se dispara una sola ves, al poner el  [] como segundo argumento
+
+    useEffect(() =>{
+        console.log('Form State cambio');
+    }, [formState]);
+
+    useEffect(() =>{
+        console.log('El email cambio');
+    }, [email]);
+
+    return (
+        <>
+            <h1>useEffect Simple Form</h1>
+            <hr />
+
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="tu nombre" 
+                    autoComplete="off"
+                    value={name}
+                    name="name"
+                    onChange={ handleInputChange }
+                    />
+            </div>
+
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="email@gmail.com" 
+                    autoComplete="off"
+                    value={email}
+                    name="email"
+                    onChange={ handleInputChange }
+                    />
+            </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 import './index.css';
 
 ReactDOM.render(
-    <CounterWithCustomHook />,
+    <SimpleForm />,
   document.getElementById('root')
 );
