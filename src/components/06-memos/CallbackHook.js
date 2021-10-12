39fe815a843347ef16e5120ac65b07498eb7f673
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-12 19:42:08 -0400

    Implemented useCallback and React.memo

---
+++ src/components/06-memos/CallbackHook.js
@@ -0,0 +1,25 @@
+import React, { useCallback, useState } from 'react';
+import '../02-useEffect/effects.css';
+import { ShowIncrease } from './ShowIncrease';
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState(10);
+
+    // const  increment = () => {
+    //     setCounter( counter + 1);
+    // }
+
+    const increment = useCallback(() => {
+        // setCounter(counter + 1);
+        setCounter( c => c + 1); //elimina la dependencia 
+    }, [setCounter]);
+
+    return (
+        <div>
+            <h1>Callback Hook: {counter}</h1>
+            <hr />
+            <ShowIncrease increment={increment} />
+        </div>
+    )
+}
---
+++ src/components/06-memos/ShowIncrease.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Para usar el useCallback este component debe estar en React.memo ( ... )
+export const ShowIncrease = React.memo(({increment}) => {
+
+    console.log('Me llamaste de nuevo :*(');
+
+    return (
+        <>
+            <button className="btn btn-primary"
+                onClick={() => {increment()}}
+            >
+                +1 Incrementar
+            </button>
+        </>
+    )
+});
