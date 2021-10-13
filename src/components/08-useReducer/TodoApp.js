9ef010e66f11285568a0662fa0a8225e3a44f118
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-13 05:53:23 -0400

    implemented localStorage to add and get initial state

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,33 +1,55 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm';
 import './styles.css';
 
 
-const initialState = [
-    {
-        id: new Date().getTime(),
-        desc: 'Aprender React',
-        done: false
-    },
-    {
-        id: new Date().getTime() + 1,
-        desc: 'Aprender Git',
-        done: false
-    },
-];
+// Funcion init para useReducer ultimo argumento 
+const init = () =>{
+    // return [
+    //     {
+    //         id: new Date().getTime(),
+    //         desc: 'Aprender React',
+    //         done: false
+    //     },
+    //     {
+    //         id: new Date().getTime() + 1,
+    //         desc: 'Aprender Git',
+    //         done: false
+    //     },
+    // ];
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
     // El dispatch es una function y sabe cuales funciones tienes el reducer que envio
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
-    console.log(todos);
+    // const [todos, dispatch] = useReducer(todoReducer, initialState);
+    // const [todos, dispatch] = useReducer(todoReducer, initialState, init); //me ayyda a comptuar todo el estado incial, function comun y corriente
+    const [todos, dispatch] = useReducer(todoReducer, [], init); 
+
+    // const [formValue, handleInputChange] = useForm({
+    const [{description}, handleInputChange, reset] = useForm({
+        description: ''
+    });
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos)); //nose puede guardar objetos.
+    }, [todos]);
+
+    console.log(description);
 
     const handleSubmit = (e) => {
-        
         e.preventDefault();
+
+        if(description.trim().length <= 1) {
+            return;
+        }
+
         const newTodo = {
             id: new Date().getTime(),
-            desc: 'Aprender React',
+            // desc: 'Aprender React',
+            desc: description,
             done: false
         };
 
@@ -37,6 +59,7 @@ export const TodoApp = () => {
         }
 
         dispatch(action);
+        reset();
 
     }
 
@@ -67,6 +90,8 @@ export const TodoApp = () => {
                             placeholder='Aprender...' 
                             autoComplete='off'  
                             className='form-control'
+                            value={description}
+                            onChange={handleInputChange}
                             />
                         <button className="btn btn-outline-primary btn-block mt-2">
                             Agregar
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,6 +5,10 @@ export const useForm = (initialState = {}) => {
 
     const [values, setValues] = useState(initialState);
 
+    const reset = () => {
+        setValues(initialState);
+    }
+
     const handleInputChange = ({target}) => {
         setValues({
             ...values,
@@ -12,5 +16,5 @@ export const useForm = (initialState = {}) => {
         })
     }
     // Retorna un array 
-    return [values, handleInputChange];
+    return [values, handleInputChange, reset];
 }
