834732b9f4de9f8d8158fa1f105c112cdf4db58d
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-14 15:47:52 -0400

    Refactored toggle option on reducer

--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -2,24 +2,31 @@
 //si no tengo valor inciial para el state, se incia con un []
 export const todoReducer = (state = [], action) => {
     switch (action.type) {
-        
+
         case 'add':
             return [...state, action.payload];
-        
+
         case 'delete':
-            return state.filter( todo => todo.id !== action.payload);
+            return state.filter(todo => todo.id !== action.payload);
 
         case 'toggle':
-            return state.map( (todo, index) => {
+            return state.map(todo =>
+                (todo.id === action.payload)
+                    ? { ...todo, done: !todo.done }
+                    : todo
+            );
+
+        case 'toggle-old':
+            return state.map((todo, index) => {
                 // Devuelve todos los todo, si se da la condicion entonces 
                 //  coge todas las propeidades del todo, pero edita el done, y deveuvle el obj todo 
                 //con la propiedad modificada
-                if( todo.id === action.payload) {
+                if (todo.id === action.payload) {
                     return {
                         ...todo, // todas las propiedades 
                         done: !todo.done
                     }
-                }else {
+                } else {
                     //Caso contrario , solo devuelve el todo. 
                     return todo;
                 }
