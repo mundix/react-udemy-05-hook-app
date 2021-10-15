e38f01010ff5d75432c01f88db6cab103be8da95
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-15 03:13:13 -0400

    Defined Route links and NavBar with Link Component

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -3,20 +3,27 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect
 } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
         <Router>
             {/* Se recomienda usar el div  */}
             <div>
+                <NavBar/>
                 <Switch>
                     <Route exact path='/' component={HomeScreen} />
                     <Route exact path='/about' component={AboutScreen} />
                     <Route exact path='/login' component={LoginScreen} />
+                    {/* Default page o 404 o usar un redirect  */}
+                    {/* <Route component={HomeScreen}/> */}
+                    {/* Si no encuentra ninguna de esatas condiciones redirect to / */}
+                    <Redirect to='/'/>
                 </Switch>
             </div>
         </Router>
