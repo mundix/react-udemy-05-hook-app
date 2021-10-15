86b047c9290bc1e71afd53a4dafd64e91895045d
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-15 04:57:14 -0400

    Update useFetch hook add catch for handle errors

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -29,6 +29,13 @@ export const useFetch = (url) => {
                     }
                 // }, 4000);
 
+            })
+            .catch(() =>{
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se puso cargar la info'
+                })
             });
     }, [url]);
 
---
+++ src/tests/hooks/useFetch.test.js
@@ -0,0 +1,34 @@
+
+import { renderHook } from '@testing-library/react-hooks';
+import { useFetch } from '../../hooks/useFetch';
+
+describe('Pruebas en useFetch', () => {
+
+    test('Debe retornar la informacion por defecto', () => {
+
+        const url = `https://www.breakingbadapi.com/api/quotes/1`;
+        const { result } = renderHook(() => useFetch(url));
+
+        // Esto es syncrono
+        const { data, loading, error } = result.current;
+        expect(data).toBe(null);
+        expect(loading).toBe(true);
+        expect(error).toBe(null);
+
+    });
+
+    test('Debe tener la info deseada', async() => {
+
+        const url = `https://www.breakingbadapi.com/api/quotes/1`;
+        const { result, waitForNextUpdate } = renderHook(() => useFetch(url));
+        await waitForNextUpdate(); //esta es una funcion sync 
+
+        // Esto es syncrono
+        const { data, loading, error } = result.current;
+        expect(data.length).toBe(1);
+        expect(loading).toBe(false);
+        expect(error).toBe(null);
+    })
+
+
+})
