a4ec69731be1cb0fd0c2475739213c5eb55369d9
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-15 03:05:00 -0400

    Defined routes and screens

--- src/index.js
+++ src/index.js
@@ -13,12 +13,13 @@ import ReactDOM from 'react-dom';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
 import './index.css';
 
 ReactDOM.render(
-    <TodoApp />,
+    <MainApp />,
   document.getElementById('root')
 );
-// import './components/08-useReducer/intro-reducer';
+// import './components/08-useReducer/intro-reducer';import { MainApp } from './components/09-useContext/MainApp';
