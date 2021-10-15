3a6ef0d248a3effd5da542c900e76466c12ae579
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-15 03:26:05 -0400

    Use NavLink on NavBar implementation with properties activeClass and exact

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink} from 'react-router-dom';
 
 export const NavBar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to='/'>useContext</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink exact activeClassName='active' to='/' className='nav-link'>Home</NavLink>
+                    <NavLink exact activeClassName='active' to='/about' className='nav-link '>About</NavLink>
+                    <NavLink exact activeClassName='active' to='/login' className='nav-link '>Login</NavLink>
+                </div>
+            </div>
         </nav>
     )
 }
